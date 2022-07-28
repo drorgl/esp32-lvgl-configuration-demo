2dd9a0ec9ff5fd97ea276d749ca665b7f2daf069
Dror Gluska <drorgl@yahoo.com>
2022-07-28 23:04:46 +0300

    enable lvgl demo widget increase lvgl drivers buffer size to 16384 fix brightness and RGB565 conversion for ili9488 fix build issues in lv_drivers

--- lib/lvgl_esp32_drivers/lvgl_tft/ili9488.c
+++ lib/lvgl_esp32_drivers/lvgl_tft/ili9488.c
@@ -55,21 +55,24 @@ static void ili9488_send_color(void * data, uint16_t length);
 void ili9488_init(void)
 {
 	lcd_init_cmd_t ili_init_cmds[]={
+		{ILI9488_CMD_SOFTWARE_RESET, {0x01}, 0x80},
                 {ILI9488_CMD_SLEEP_OUT, {0x00}, 0x80},
-		{ILI9488_CMD_POSITIVE_GAMMA_CORRECTION, {0x00, 0x03, 0x09, 0x08, 0x16, 0x0A, 0x3F, 0x78, 0x4C, 0x09, 0x0A, 0x08, 0x16, 0x1A, 0x0F}, 15},
-		{ILI9488_CMD_NEGATIVE_GAMMA_CORRECTION, {0x00, 0x16, 0x19, 0x03, 0x0F, 0x05, 0x32, 0x45, 0x46, 0x04, 0x0E, 0x0D, 0x35, 0x37, 0x0F}, 15},
-		{ILI9488_CMD_POWER_CONTROL_1, {0x17, 0x15}, 2},
-		{ILI9488_CMD_POWER_CONTROL_2, {0x41}, 1},
-		{ILI9488_CMD_VCOM_CONTROL_1, {0x00, 0x12, 0x80}, 3},
+		{ILI9488_CMD_POWER_CONTROL_1, {0x01, 0x01}, 2}, //brightness (low/high 0x00 - 0x1F)
+		{ILI9488_CMD_POWER_CONTROL_2, {0x41}, 1}, //brightness multiplier 0x40 - 0x47
+		{ILI9488_CMD_POWER_CONTROL_NORMAL_3, {0x00},1},
+		// {ILI9488_CMD_VCOM_CONTROL_1, {0x00, 0x12, 0x80}, 3},
+		{ILI9488_CMD_VCOM_CONTROL_1, {0x00, 0x18, 0x80}, 3}, //brightness (2nd byte)
 		{ILI9488_CMD_MEMORY_ACCESS_CONTROL, {(0x20 | 0x08)}, 1},
-		{ILI9488_CMD_COLMOD_PIXEL_FORMAT_SET, {0x66}, 1},
+		{ILI9488_CMD_COLMOD_PIXEL_FORMAT_SET, {0b0110110}, 1},//24bits //24bits
 		{ILI9488_CMD_INTERFACE_MODE_CONTROL, {0x00}, 1},
 		{ILI9488_CMD_FRAME_RATE_CONTROL_NORMAL, {0xA0}, 1},
 		{ILI9488_CMD_DISPLAY_INVERSION_CONTROL, {0x02}, 1},
 		{ILI9488_CMD_DISPLAY_FUNCTION_CONTROL, {0x02, 0x02}, 2},
 		{ILI9488_CMD_SET_IMAGE_FUNCTION, {0x00}, 1},
 		{ILI9488_CMD_WRITE_CTRL_DISPLAY, {0x28}, 1},
-		{ILI9488_CMD_WRITE_DISPLAY_BRIGHTNESS, {0x7F}, 1},
+		{ILI9488_CMD_WRITE_CONTENT_ADAPT_BRIGHTNESS, {0x00},1},
+		{ILI9488_CMD_WRITE_DISPLAY_BRIGHTNESS, {0x0F}, 1},
+		// {ILI9488_CMD_WRITE_DISPLAY_BRIGHTNESS, {0xFF}, 1},
 		{ILI9488_CMD_ADJUST_CONTROL_3, {0xA9, 0x51, 0x2C, 0x02}, 4},
 		{ILI9488_CMD_DISPLAY_ON, {0x00}, 0x80},
 		{0, {0}, 0xff},
@@ -103,6 +106,8 @@ void ili9488_init(void)
 		ili9488_send_data(ili_init_cmds[cmd].data, ili_init_cmds[cmd].databytes&0x1F);
 		if (ili_init_cmds[cmd].databytes & 0x80) {
 			vTaskDelay(100 / portTICK_RATE_MS);
+		}else{
+			vTaskDelay(5 / portTICK_RATE_MS);
 		}
 		cmd++;
 	}
@@ -110,15 +115,23 @@ void ili9488_init(void)
     ili9488_set_orientation(CONFIG_LV_DISPLAY_ORIENTATION);
 }
 
+uint8_t convert_8_to_6_bits(uint8_t val){
+		return ((uint32_t)val * 253 + 512) >> 10;
+}
+
 // Flush function based on mvturnho repo
 void ili9488_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map)
 {
     uint32_t size = lv_area_get_width(area) * lv_area_get_height(area);
 
+#if LV_COLOR_DEPTH == 16
     lv_color16_t *buffer_16bit = (lv_color16_t *) color_map;
+#elif  LV_COLOR_DEPTH == 32
+	lv_color32_t *buffer_32bit = (lv_color32_t *) color_map;
+#endif
     uint8_t *mybuf;
     do {
-        mybuf = (uint8_t *) heap_caps_malloc(3 * size * sizeof(uint8_t), MALLOC_CAP_DMA);
+        mybuf = (uint8_t *) heap_caps_malloc(3 * size * sizeof(uint8_t) + 3, MALLOC_CAP_DMA);
         if (mybuf == NULL)  ESP_LOGW(TAG, "Could not allocate enough DMA memory!");
     } while (mybuf == NULL);
 
@@ -126,13 +139,24 @@ void ili9488_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * col
     uint32_t j = 0;
 
     for (uint32_t i = 0; i < size; i++) {
-        LD = buffer_16bit[i].full;
-        mybuf[j] = (uint8_t) (((LD & 0xF800) >> 8) | ((LD & 0x8000) >> 13));
-        j++;
-        mybuf[j] = (uint8_t) ((LD & 0x07E0) >> 3);
-        j++;
-        mybuf[j] = (uint8_t) (((LD & 0x001F) << 3) | ((LD & 0x0010) >> 2));
-        j++;
+
+#if LV_COLOR_DEPTH == 16
+		mybuf[j] = buffer_16bit[i].ch.red << 1;
+		j++;
+		mybuf[j] = buffer_16bit[i].ch.green << 0;
+		j++;
+		mybuf[j] = buffer_16bit[i].ch.blue << 1;
+		j++;
+
+#elif  LV_COLOR_DEPTH == 32
+		mybuf[j] = convert_8_to_6_bits(buffer_32bit[i].ch.red);
+		j++;
+		mybuf[j] = convert_8_to_6_bits(buffer_32bit[i].ch.green);
+		j++;
+		mybuf[j] = convert_8_to_6_bits(buffer_32bit[i].ch.blue);
+		j++;
+#endif
+
     }
 
 	/* Column addresses  */
@@ -164,6 +188,8 @@ void ili9488_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * col
 
 	ili9488_send_color((void *) mybuf, size * 3);
 	heap_caps_free(mybuf);
+
+	lv_disp_flush_ready(drv);
 }
 
 /**********************
--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 
 #include <stdio.h>
 
+#include <demos/widgets/lv_demo_widgets.h>
+
 MAIN()
 {
     printf("starting...\r\n");
@@ -16,20 +18,8 @@ MAIN()
     /*Initialize the HAL (display, input devices, tick) for LVGL*/
     hal_setup();
 
-    /* use a pretty small demo for monochrome displays */
-    /* Get the current screen  */
-    lv_obj_t *scr = lv_disp_get_scr_act(NULL);
 
-    /*Create a Label on the currently active screen*/
-    lv_obj_t *label1 = lv_label_create(scr);
-
-    /*Modify the Label's text*/
-    lv_label_set_text(label1, "Hello\nworld");
-
-    /* Align the Label to the center
-     * NULL means align on parent (which is the screen now)
-     * 0, 0 at the end means an x, y offset after alignment*/
-    lv_obj_align(label1, LV_ALIGN_CENTER, 0, 0);
+    lv_demo_widgets();
 
     hal_loop();
 
